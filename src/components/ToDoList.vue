4121a90e6d5c7110295e97961dfe86441bf8dddd
Saza-ku <the.latticeheart@gmail.com>
2021-08-11 23:31:10 +0900

    p51 入力フォームの実装まで

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <input v-model="inputValue">
+    入力した値: {{ inputValue }}
+  <ul>
+    <li v-for="todo in todoItems" v-bind:key="todo.id">
+      {{ todo.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputValue: '',
+      todoItems: [
+        { id: 1, text: 'Go out to sea' },
+        { id: 2, text: 'Invite the first member' }        
+      ]
+    }
+  }
+}
+</script>
