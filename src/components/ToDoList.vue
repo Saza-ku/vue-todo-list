bf3761f80a9f4d342b00fc4914e4781a0dd78204
Saza-ku <the.latticeheart@gmail.com>
2021-09-09 23:16:59 +0900

    p56 todo list 完成まで

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,17 +1,29 @@
 <template>
-  <input v-model="inputValue">
   <button v-on:click="handleClick">
     ToDo を追加
   </button>
+  <input v-model="inputValue">
+  <input
+    v-model="filterValue"
+    placeholder="フィルタテキスト">
   <ul>
-    <li v-for="todo in todoItems"
+    <li
+      v-for="todo in filterdTodoItems"
       v-bind:key="todo.id"
+      class="todo-item"
+      v-bind:class="{'done': todo.done}"
       v-on:click="todo.done = !todo.done">
       <span v-if="todo.done">✔︎</span>
       {{ todo.text }}
     </li>
   </ul>
 </template>
+
+<style>
+.todo-item.done {
+  background-color: #3fb983;
+}
+</style>
 
 <script>
 export default {
@@ -28,8 +40,19 @@ export default {
           done: false,
           text: 'Invite the first member'
         }
-      ]
+      ],
+      filterValue: '',
     }
+  },
+  computed: {
+    filterdTodoItems() {
+      if (!this.filterValue) {
+        return this.todoItems
+      }
+      return this.todoItems.filter((todo) => {
+        return todo.text.includes(this.filterValue)
+      })
+    },
   },
   methods: {
     handleClick() {
@@ -41,7 +64,7 @@ export default {
       })
       // 入力をクリア
       this.inputValue = ''
-    }
+    },
   }
 }
 </script>
